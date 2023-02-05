03f6cb670c374368650a5b551df133f724b0b098
Christian Nikel <chnikel@gmail.com>
2023-02-05 17:50:23 +0100

    Replace i3blocks with polybar

--- link.sh
+++ link.sh
@@ -1,5 +1,11 @@
-rm ~/.config/i3/config
-ln -s ~/repos/dotfiles/i3/config ~/.config/i3/config
+# Arg1 Path in dotfiles repo
+# Arg2 Path on system
+link () {
+    rm $2
+    ln -s $1 $2
+    echo "Linked $2 -> $1"
+}
 
-rm ~/.zshrc
-ln -s ~/repos/dotfiles/.zshrc ~/.zshrc
+link ~/repos/dotfiles/i3/config             ~/.config/i3/config
+link ~/repos/dotfiles/.zshrc                ~/.zshrc
+link ~/repos/dotfiles/polybar/config.ini    ~/.config/polybar/config.ini
